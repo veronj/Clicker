d71833b50cc189ec7b7531ce130489c91b6092cd
veronj <veron_j@etna-alternance.net>
2018-10-27 19:05:44 +0200

    fix

--- app/Http/Controllers/ResourcesController.php
+++ app/Http/Controllers/ResourcesController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class resources extends Controller
+class ResourcesController extends Controller
 {
     /**
      * Display a listing of the resource.

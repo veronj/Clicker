9026e48fc2336bdebcd0724fa157d53cbc4a0311
veronj <veron_j@etna-alternance.net>
2018-10-27 19:29:16 +0200

    resources

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,9 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->integer('food');
+            $table->integer('metal');
+            $table->integer('coal');
             $table->rememberToken();
             $table->timestamps();
         });
